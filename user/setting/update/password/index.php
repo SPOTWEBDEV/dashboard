266a4e9ebd01e92c457b72525b31f4ff6354b595
SPOTWEBDEV <spotwebdev.com@gmail.com>
2024-01-04 19:59:01 +0100

    working on the edit password

--- user/setting/update/password/index.php
+++ user/setting/update/password/index.php
@@ -2,6 +2,8 @@
 
 include('../../../../server/database.php');
 
+
+
 ?>
 
 
@@ -13,6 +15,7 @@ include('../../../../server/database.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 <style>
     #crid_mode {
@@ -21,6 +24,8 @@ include('../../../../server/database.php');
 </style>
 
 <body>
+
+
 
 
     <?php include('../../../../layout/client/nav.php') ?>
@@ -48,28 +53,68 @@ include('../../../../server/database.php');
 
                         </div>
                         <!-- Modal body -->
-                        <div class="p-4 md:p-5">
+
+                        <?php
+
+
+                        if (isset($_POST['change'])) {
+                            $old_password = $_POST['old_password'];
+                            $cpassword = $_POST['cpassword'];
+                            $new_password = $_POST['new_password'];
+                            $stored_password = $_POST['password'];
+
+                            if (
+                                $old_password == '' && $cpassword == '' && $new_password == ''
+                            ) {
+                                echo '<script>alert("Input can\'t be empty")</script>';
+                            } else {
+                                if ($old_password == $stored_password) {
+                                    if ($cpassword == $new_password) {
+                                        $update_query = "UPDATE `clients` SET `password` = '$new_password' WHERE `id` = $id";
+                                        $update_result = mysqli_query($connection, $update_query);
+
+                                        if ($update_result) {
+                                            echo "<script>alert('Password updated')</script>";
+                                        } else {
+                                            echo "<script>alert('Error updating password')</script>";
+                                        }
+                                    } else {
+                                        echo "<script>alert('New password and confirm password do not match')</script>";
+                                    }
+                                } else {
+                                    echo "<script>alert('Incorrect old password')</script>";
+                                }
+                            }
+                        }
+
+
+
+                        ?>
+
+
+                        <form method="POST" class="p-4 md:p-5">
                             <div class="grid gap-4 mb-4 grid-cols-2">
                                 <div class="col-span-2">
                                     <label for="name" class="transferInput block mb-2 text-sm font-medium text-gray-900 dark:text-white ">Old password</label>
-                                    <input type="text" name="name" id="name" class="account_number bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type Old password" required="">
+                                    <input type="hidden" class="initailpassword" name="password">
+                                    <input type="text" name="old_password" id="name" class=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type Old password" required="">
                                 </div>
                                 <div class="col-span-2">
                                     <label for="name" class="transferInput block mb-2 text-sm font-medium text-gray-900 dark:text-white ">New Password</label>
-                                    <input type="text" name="name" id="name" class="account_name bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type New Password" required="">
+                                    <input type="text" name="new_password" id="name" class=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type New Password" required="">
                                 </div>
                                 <div class="col-span-2 ">
                                     <label for="price" class="transferInput block mb-2 text-sm font-medium text-gray-900 dark:text-white ">Confirm New Password</label>
-                                    <input type="text" name="price" id="price" class="amounts bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Confirm New Password" required>
+                                    <input type="text" name="cpassword" id="price" class=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Confirm New Password" required>
                                 </div>
 
 
                             </div>
-                            <button onclick="checkInputs()" id=" button" type=" button" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            <button id="button" type="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" name="change">
 
                                 Change
                             </button>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
@@ -88,14 +133,15 @@ include('../../../../server/database.php');
 
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js"></script>
 
     <script>
         let domain = "<?php echo $domain ?>";
 
         function value(data) {
-             console.log(data[0].email);
+            $('.initailpassword').val(data[0].password)
+            console.log(data[0].email);
         }
     </script>
     <script src="../../../../assets/js/localstore.js"></script>
