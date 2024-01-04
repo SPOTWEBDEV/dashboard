09d4a3df58ac317850d0475aae834ac9cc0640f4
SPOTWEBDEV <spotwebdev.com@gmail.com>
2024-01-04 11:38:18 +0100

    working on the transfer

--- user/transfer/index.php
+++ user/transfer/index.php
@@ -39,15 +39,15 @@
                                                       <form class="p-4 md:p-5">
                                                                <div class="grid gap-4 mb-4 grid-cols-2">
                                                                         <div class="col-span-2">
-                                                                                 <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Account Number</label>
+                                                                                 <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white account_number">Account Number</label>
                                                                                  <input type="number" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type Account Number" required="">
                                                                         </div>
                                                                         <div class="col-span-2">
-                                                                                 <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Account Name</label>
+                                                                                 <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white account_name">Account Name</label>
                                                                                  <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type Account Name" required="">
                                                                         </div>
                                                                         <div class="col-span-2 ">
-                                                                                 <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Amount</label>
+                                                                                 <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white amounts">Amount</label>
                                                                                  <input type="number" name="price" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Enter Amount" required>
                                                                         </div>
 
