d8541ec856ffa0d24637d7bf93aff4056b512d99
CharlesBenedict123 <anusicharles392@gmail.com>
2024-01-04 20:26:36 +0100

    working on admin login

--- layout/admin/navbar.php
+++ layout/admin/navbar.php
@@ -1,4 +1,4 @@
-<nav class="bg-white border-gray-200 dark:bg-gray-900">
+<nav class="bg-white border-gray-200 dark:bg-gray-900" id="ad_back">
          <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
                   <a href="https://flowbite.com" class="flex items-center space-x-3 rtl:space-x-reverse">
 
