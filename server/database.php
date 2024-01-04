8db7d28efe14135be3e07a2ece7f6917575c841a
SPOTWEBDEV <spotwebdev.com@gmail.com>
2024-01-04 09:18:23 +0100

    added database file

--- server/database.php
+++ server/database.php
@@ -1,5 +1,14 @@
 <?php
+define("HOST", "localhost");
+define("USER", "root");
+define("PASSWORD", "");
+define("DATABASE", "indusindnet");
 
+$connection = mysqli_connect(HOST, USER, PASSWORD, DATABASE);
+
+if (!$connection) {
+         die('not conneted to databse');
+}
 
 $domain = "http://localhost/dashboards";
 ?>
