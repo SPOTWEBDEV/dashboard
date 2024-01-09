ee5eb6592253bcc6f754bb53a88a4b64f98c863e
SPOTWEBDEV <spotwebdev.com@gmail.com>
2024-01-10 00:32:17 +0100

    working on the fix error

--- server/config.php
+++ server/config.php
@@ -20,5 +20,4 @@
          }
 </style>
 
-
-
+<script src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
--- user/dashboard/index.php
+++ user/dashboard/index.php
@@ -419,10 +419,10 @@ include('../../server/config.php');
 
     <script type="text/javascript" src="../../assets/js//tawk.js"></script>
 
-    
 
 
 
+    <script src="<?php echo $domain ?>assets/js/translate.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js"></script>
 </body>
 
